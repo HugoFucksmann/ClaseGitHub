9bd67d2388fad6ee6e27fe537d14282aa7c6ac04
hugo.fuckmann <hugo.fuckman@errepar.com>
2023-02-01 21:20:13 -0300

    fix bugs

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ let ul = document.getElementById("listMoves");
 
 async function getPikachu() {
   const pikachu = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then((res) => {
-      if (res.status !== 200) throw Error;
-
-      return res.json();
-    })
+    .then((res) => res.json())
     .catch((e) => {
       console.warn("El error es: ", e);
       return null;
